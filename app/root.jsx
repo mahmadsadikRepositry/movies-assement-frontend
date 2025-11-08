20f9d8013cb03166c7370a8ef24339c64679fa82
mahmadsadikRepositry <sadikkoliya@gmail.com>
2025-11-08 16:57:08 -0500

    Installed Dependency and Removed bolilerplate code.

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,4 @@
 import {
-  isRouteErrorResponse,
   Meta,
   Outlet,
   Scripts,
@@ -30,31 +29,3 @@ export default function App() {
   return <Outlet />;
 }
 
-export function ErrorBoundary({ error }) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
-}
